6bec2ebd841661b16bfdfd1ca251849293a6b8bd
Marcos Minond <minond.marcos@gmail.com>
2016-08-20 17:10:10 -0600

    fixing asserts

--- responses.js
+++ responses.js
@@ -4,13 +4,13 @@ var REGEX_I_AM_NAME_EXTRACT = /(^|\W)i[\'m|\s{0,}am]{0,}(.+)/i;
 
 var test = require('./utils').test;
 
-test(hi_i_am_dad('junk'), false);
-test(hi_i_am_dad('im hungry'), 'Hi hungry, I\'m dad.');
-test(hi_i_am_dad('i\'m hungry'), 'Hi hungry, I\'m dad.');
-test(hi_i_am_dad('i am hungry'), 'Hi hungry, I\'m dad.');
-test(hi_i_am_dad('and i am hungry'), 'Hi hungry, I\'m dad.');
-test(hi_i_am_dad('   and    i am hungry'), 'Hi hungry, I\'m dad.');
-test(hi_i_am_dad('i  am  hungry'), 'Hi hungry, I\'m dad.');
+test(hi_i_am_dad('junk') === false);
+test(hi_i_am_dad('im hungry') === 'Hi hungry, I\'m dad.');
+test(hi_i_am_dad('i\'m hungry') === 'Hi hungry, I\'m dad.');
+test(hi_i_am_dad('i am hungry') === 'Hi hungry, I\'m dad.');
+test(hi_i_am_dad('and i am hungry') === 'Hi hungry, I\'m dad.');
+test(hi_i_am_dad('   and    i am hungry') === 'Hi hungry, I\'m dad.');
+test(hi_i_am_dad('i  am  hungry') === 'Hi hungry, I\'m dad.');
 function hi_i_am_dad(str) {
     var matches = str.match(REGEX_I_AM_NAME_EXTRACT),
         name = matches ? matches[2] : '';
