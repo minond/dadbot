b501e69fa3b9f3892af1c0367655557696f5e2e9
Dean <dcheesman@gmail.com>
2016-08-20 15:41:22 -0600

    Added -more like...- joke function

--- index.js
+++ index.js
@@ -1,4 +1,33 @@
 'use strict';
 
-const Rita = require('rita');
-const Bitkit = require('botkit');
+if (!process.env.token) {
+    console.log('Error: Specify token in environment');
+    process.exit(1);
+}
+
+var Rita = require('rita');
+var Botkit = require('botkit');
+
+var controller = Botkit.slackbot({
+    debug: true
+});
+
+var bot = controller.spawn({
+    token: process.env.token
+}).startRTM();
+
+var lexicon = new Rita.RiLexicon();
+
+controller.hears('', 'direct_message,direct_mention,mention,ambient', function(bot, message) {
+
+        var words = message.text.split(/\s*\b\s*/);
+        for (var i = 0; i < words.length-1; i++) {
+            if( lexicon.isAdjective(words[i]) == true && words.length > 1){
+                var rhymes = lexicon.rhymes(words[i+1]);
+                if(rhymes.length > 0){
+                    var random_id = Math.random()*rhymes.length|0
+                    return bot.reply(message, "More like " + words[i] + " " + rhymes[random_id]);
+                }
+            }
+        }
+    });
