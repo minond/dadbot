b2ac08d1627c40a6b132910e8c2523c76c4e90a1
Marcos Minond <minond.marcos@gmail.com>
2016-08-20 17:45:45 -0600

    regex fix

--- responses.js
+++ responses.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var REGEX_I_AM_NAME_EXTRACT = /(^|\W)i[\'m|\s{0,}am]{0,}(.+)/i;
+var REGEX_I_AM_NAME_EXTRACT = /(^|\W)i(m|\'m|\s{0,}am)\s{0,}(.+)/i;
 
 var test = require('./utils').test;
 
@@ -13,7 +13,7 @@ test(hi_i_am_dad('   and    i am hungry') === 'Hi hungry, I\'m dad.');
 test(hi_i_am_dad('i  am  hungry') === 'Hi hungry, I\'m dad.');
 function hi_i_am_dad(str) {
     var matches = str.match(REGEX_I_AM_NAME_EXTRACT),
-        name = matches ? matches[2] : '';
+        name = matches ? matches[3] : '';
 
     return name ? 'Hi ' + name + ', I\'m dad.' : false;
 }
