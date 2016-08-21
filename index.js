c4b383eaf5c2f8ca79b482436fe6bfd9f2015487
Dean <dcheesman@gmail.com>
2016-08-20 18:18:25 -0600

    look for a random gif based on nouns in a sentence

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ if (!process.env.token) {
 var Rita = require('rita');
 var Botkit = require('botkit');
 var responses = require('./responses');
+var request = require('request');
 
 var controller = Botkit.slackbot({
     debug: process.env.DEBUG || process.env.debug
@@ -29,13 +30,31 @@ controller.hears('(^|\W)i[\'m|\s{0,}am]{0,}(.+)', 'direct_message,direct_mention
 
 controller.hears('', 'direct_message,direct_mention,mention,ambient', function(bot, message) {
     var words = message.text.split(/\s*\b\s*/);
-    for (var i = 0; i < words.length-1; i++) {
+    var nouns = [];
+    for (var i = 0; i < words.length; i++) {
         if( lexicon.isAdjective(words[i]) == true && words.length > 1){
             var rhymes = lexicon.rhymes(words[i+1]);
             if(rhymes.length > 0){
                 var random_id = Math.random()*rhymes.length|0
-                bot.reply(message, "More like " + words[i] + " " + rhymes[random_id]);
+                return bot.reply(message, "More like " + words[i] + " " + rhymes[random_id]);
             }
+        } else if (lexicon.isNoun(words[i])){
+            nouns.push(words[i]);
         }
     }
+    if(nouns.length > 0){
+        var gif_url = post_a_gif(nouns, message);
+    }
 });
+
+function post_a_gif (keywords, message) {
+    console.log("Searching keywords: " + keywords.join(","));
+    var url = 'http://api.giphy.com/v1/gifs/search?q=' + keywords.join('+') + '&api_key=dc6zaTOxFJmzC&rating=pg-13';
+    request.get(url, function (err, res, body) {
+        var data = JSON.parse(body)['data'];
+        if(data.length > 0){
+            var random_id = Math.random() * data.length | 0;
+            return bot.reply(message, data[random_id].bitly_url);
+        }
+    })
+}
