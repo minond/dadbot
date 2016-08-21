2bcc7c6d27efdb2997cd5135a6b5ff78ac1ca75d
Marcos Minond <minond.marcos@gmail.com>
2016-08-20 21:22:22 -0600

    joke comback generator

--- responses.js
+++ responses.js
@@ -2,6 +2,8 @@
 
 var REGEX_I_AM_NAME_EXTRACT = /(^|\W)i(m|\'m|\s{0,}am)\s{0,}(.+)/i;
 
+var fs = require('fs');
+var Rita = require('rita');
 var test = require('./utils').test;
 
 test(hi_i_am_dad('junk') === false);
@@ -18,6 +20,36 @@ function hi_i_am_dad(str) {
     return name ? 'Hi ' + name + ', I\'m dad.' : false;
 }
 
+test(find_noun('where is the') === false);
+test(find_noun('where is the beef') === 'beef');
+test(find_noun('where are the beef stores') === 'beef');
+test(find_noun('where are the stores') === 'stores');
+function find_noun(str) {
+    var ristr = Rita.RiString(str),
+        pos1 = ristr.pos().indexOf('nn'),
+        pos2 = ristr.pos().indexOf('nns');
+
+    return ristr.wordAt(pos1) || ristr.wordAt(pos2) || false;
+}
+
+test(best_joke_comeback('cheese'), 'What cheese can never be yours? Nacho cheese.');
+test(best_joke_comeback('fish'), 'What did the fish say when it swam into a wall? Damn!');
+test(best_joke_comeback('zoo'), 'I went to the zoo the other day, there was only one dog in it. It was a shitzu.');
+function best_joke_comeback(str) {
+    if (!best_joke_comeback.jokes) {
+        best_joke_comeback.jokes = fs.readFileSync('./jokes.txt').toString().split('\n');
+        best_joke_comeback.jdict = best_joke_comeback.jokes.reduce(function (store, joke) {
+            try {
+                store[find_noun(joke)] = joke;
+            } catch (ignore) {}
+            return store;
+        }, {});
+    }
+
+    return best_joke_comeback.jokes[3];
+}
+
 module.exports = {
     hi_i_am_dad: hi_i_am_dad,
+    best_joke_comeback: best_joke_comeback,
 };
